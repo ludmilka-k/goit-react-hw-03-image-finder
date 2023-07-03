a5c0beb7266c08109936c4d6ae1da60ad6916690
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2023-07-03 20:46:46 +0300

    task image finder

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from 'components/App/App';
+import { ThemeProvider } from '@emotion/react'
+import {theme} from './constans';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
+
 );
